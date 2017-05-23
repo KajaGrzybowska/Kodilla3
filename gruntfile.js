9e907eec4a8aa4d001adbc244d1a04c51a31035e
Kaja Grzybowska <kajagrzybowska@outlook.com>
2017-05-23 20:05:52 +0200

    modified gitignore, sass-css, imagine min, watch

--- gruntfile.js
+++ gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
   		},
   		dist: {
   			files: {
-  				'main.css': 'main.sass'
+  				'style/main.css': 'sass/main.sass'
   			}
   		}
   	},
@@ -38,8 +38,9 @@ module.exports = function(grunt) {
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   
-  grunt.registerTask('default', ['sass', 'imagemin']);
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
 };
